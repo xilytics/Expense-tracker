5e86ec15a2ed9960fe00c146217e0a187e485c39
xiyang <xiyang0605@outlook.com>
2024-07-19 11:42:49 +0200

    Updating frontend configapi

--- frontend/src/components/signin/SignIn.js
+++ frontend/src/components/signin/SignIn.js
@@ -3,7 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import styles from './SignIn.module.css'; 
 import waveIcon from '../visuals/wave.svg';
-import configapi from '../configapi'; 
+import configapi from '../../configapi'; 
 
 
 const SignIn = () => {
